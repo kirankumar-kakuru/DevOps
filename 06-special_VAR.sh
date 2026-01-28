40bc7efb8e9b9ee1f22202cff5e07c349647b060
Kiran <kiranwls6@gmail.com>
2026-01-28 15:50:07 -0800

    test

---
+++ 06-special_VAR.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#### SPECIAL VARIABLES ####
+
+echo "All args passed to script: $@"
+echo "Number of variables passed to script: $#"
+echo "script name: $0"
+echo "Present directory: $PWD"
+echo "who is running: $USER"
+echo "home Directory: $HOME"
+echo "PID of the script: $$"
+sleep 20 &
+echo "background PID: $!"
+echo all arguments paased to script: $*"

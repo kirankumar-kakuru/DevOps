78645ac05576d83802b70ddb1ecff458425a9f22
Kiran <kiranwls6@gmail.com>
2026-01-28 15:31:27 -0800

    test

--- 05-var-time-diff.sh
+++ 05-var-time-diff.sh
@@ -5,6 +5,6 @@ START_TIME=$(date +%s)
 echo "script executed on $START_TIME"
 
 END_TIME=$(date +s%)
-TOTAL_TIME=({$END_TIME-$START_TIME})
+TOTAL_TIME=(($END_TIME-$START_TIME))
 
 echo "script executed in : $TOTAL_TIME"

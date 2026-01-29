dc0cfb362b42bea200a0cac9a5e745738a1a66ce
Kiran <kiranwls6@gmail.com>
2026-01-29 13:27:52 -0800

    test

--- 08-condition.sh
+++ 08-condition.sh
@@ -2,9 +2,8 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 20] then
-    echo "given number: $NUMBER is greater than 20"
-    
+if [$NUMBER -gt 20]; then
+    echo "given number: $NUMBER is greater than 20"  
 else
     echo "given number: $NUMBER is less than 20"
 fi

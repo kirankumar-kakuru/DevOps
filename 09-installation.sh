aa77118c507a9801f770bdd107f8e2281eca5da1
Kiran <kiranwls6@gmail.com>
2026-01-29 16:33:45 -0800

    test

--- 09-installation.sh
+++ 09-installation.sh
@@ -16,3 +16,23 @@ if [ $? -ne 0 ]; then
 else
     echo "Installing nginx ..... success"
 fi
+
+echo "Installing mysql"
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing mysql .... FAILURE"
+    exit 1
+else
+    echo "Installing mysql ..... success"
+fi
+
+echo "Installing nodejs"
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing nodejs .... FAILURE"
+    exit 1
+else
+    echo "Installing nodejs ..... success"
+fi
